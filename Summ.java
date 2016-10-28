9a06cb7577ea65d8731f13a1baeb1025cc12e67b
phannaproueng <prouengphanna195@gmail.com>
2016-10-28 08:43:37 +0700

    Create Summ.java

---
+++ Summ.java
@@ -0,0 +1,17 @@
+public class summ {
+	public static void main (String[] args)
+	{
+		int a = 0;
+		//int b =5 ;
+		int i = 1;
+		System.out.println(i);
+		for ( i = 5 ; i <= 100 ; i+=5){
+			//a = b * i ;
+			System.out.println(i );
+			a++;
+		}
+		System.out.println("Running is "+a+ " times");
+		
+	}
+
+}
